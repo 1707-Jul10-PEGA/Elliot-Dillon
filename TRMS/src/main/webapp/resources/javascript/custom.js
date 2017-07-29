b43d93a522b28ad3a51c4773117f7acb85f27bf5
Elliot Chen <ejchen145@gmail.com>
2017-07-29 12:17:54 -0400

    can add and remove additional doc. Minor bug with the order of deleting and adding them

---
+++ TRMS/src/main/webapp/resources/javascript/custom.js
@@ -0,0 +1,32 @@
+var last_doc=1;
+
+
+$(document).ready(function() {
+
+	console.log("document loaded");
+	//append nav menu
+	$.get("nav.html", function(data) {
+		$("#navbarPlaceHolder").replaceWith(data);
+	});
+	
+	//add addition doc to reimbursement forms
+	$("#add_doc").click(function(e){
+		e.preventDefault();
+		
+		var row = $('<li class="list-group-item extra_doc"></div>');
+		var label = $('<label class="sr-only">Extra Doc</label>');
+		var file = $("<input type='file' class='form-control-file col-4'/>");
+		var selection = $('<select class="form-control col-3"><option>Other</option><option>Supervisor Approval Email</option><option>Department Head Approval Email</option></select>');
+		row.append(label);
+		row.append(file);
+		row.append(selection);
+		$("#additional_doc").after(row);	
+	});
+
+	//remove the last added additional doc
+	$("#remove_doc").click(function(e){
+		e.preventDefault();
+		$(".extra_doc").last().remove();
+	});
+	
+});
