ea5f7d51bd338eb2d5f838fa2f0bba69c3dbe74e
dillonvi <dillonv087@gmail.com>
2017-07-28 21:53:24 -0400

    starting work on the backend; specifically our user story of submitting a reimbursement form

--- TRMS/src/main/java/com/revature/trms/DAO/EmployeeDAO.java
+++ TRMS/src/main/java/com/revature/trms/DAO/EmployeeDAO.java
@@ -1,16 +1,30 @@
 package com.revature.trms.DAO;
 
+import java.sql.SQLException;
+
 import com.revature.trms.objects.Employee;
 
 public interface EmployeeDAO {
+
+	public int insertEmployee(int P_ID, String firstName, String lastName, String street, String city, String state, 
+							String zip, String phone, String email, String password) throws SQLException;
+
+	// insert a form with the given information into the database
+	public int insertForm(int P_ID, String event, String format, String description, String justification, 
+							int hoursOff, String finalGrade, String status, String title) throws SQLException;
 	
-	public Employee getEmployee(String email,String password);
+	public boolean deleteEmployee();
+
+	public boolean updateEmployee();
 	
-	public Employee getEmployeeSupervisor(int e_id,String department);
+	public Employee getEmployee(String email,String password) throws SQLException;
 	
-	public Employee getDirectManager(String department);
+	public Employee getEmployeeSupervisor(int e_id,String department) throws SQLException;
 	
-	public Employee getHeadOfDepartment(String department);
+	public Employee getDirectManager(String department) throws SQLException;
 	
-	public Employee getBenco();
+	public Employee getHeadOfDepartment(String department) throws SQLException;
+	
+	public Employee getBenco() throws SQLException;
+	
 }
--- TRMS/src/main/java/com/revature/trms/DAO/EmployeeDAOImpl.java
+++ TRMS/src/main/java/com/revature/trms/DAO/EmployeeDAOImpl.java
@@ -1,8 +1,69 @@
 package com.revature.trms.DAO;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import com.revature.trms.objects.Employee;
 
 public class EmployeeDAOImpl extends DAOFactory implements EmployeeDAO {
+
+	Connection conn = null;
+	
+	// set up a connection
+	public void setup() throws SQLException {
+		conn = DAOFactory.getConnection();
+	}
+	
+	// no-args constructor
+	public EmployeeDAOImpl() throws SQLException {
+		setup();
+	}
+
+	@Override
+	public int insertEmployee(int P_ID, String firstName, String lastName, String street, String city, String state,
+								String zip, String phone, String email, String password) throws SQLException {
+		String sql = "INSERT INTO EMPLOYEE "
+					+ "(P_ID, FIRSTNAME, LASTNAME, STREET, CITY, STATE, ZIP_CODE, PHONE_NUMBER, EMAIL, PASSWORD) "
+					+ "VALUES "
+					+ "(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+		
+		PreparedStatement pStmt = conn.prepareStatement(sql);
+
+		pStmt.setInt(1, P_ID);
+		pStmt.setString(2, firstName);
+		pStmt.setString(3, lastName);
+		pStmt.setString(4, street);
+		pStmt.setString(5, city);
+		pStmt.setString(6, state);
+		pStmt.setString(7, zip);
+		pStmt.setString(8, phone);
+		pStmt.setString(9, email);
+		pStmt.setString(10, password);
+		
+		return pStmt.executeUpdate();
+	}
+
+	/* TODO: Locataion for reimbusement form is the location of the event, NOT the employee */
+	@Override
+	public int insertForm(int P_ID, String event, String format, String description, String justification, int hoursOff, 
+							String finalGrade, String status, String title) throws SQLException {
+		String sql = "INSERT INTO REIMBURSEMENT_FORMS "
+				+ "(P_ID, )"
+				+ "VALUES"
+				+ "()";
+		return 0;	
+	}
+	
+	@Override
+	public boolean deleteEmployee() {
+		return false;	
+	}
+	
+	@Override
+	public boolean updateEmployee() {
+		return false;
+	}
 
 	@Override
 	public Employee getEmployee(String email, String password) {
