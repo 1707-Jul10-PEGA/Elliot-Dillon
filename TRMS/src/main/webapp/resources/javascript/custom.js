67bf8b3093a33a61372e09752d927640b847b2ab
Elliot Chen <ejchen145@gmail.com>
2017-07-29 17:17:07 -0400

    added view for form list. cannot connect to DB

--- TRMS/src/main/webapp/resources/javascript/custom.js
+++ TRMS/src/main/webapp/resources/javascript/custom.js
@@ -29,4 +29,25 @@ $(document).ready(function() {
 		$(".extra_doc").last().remove();
 	});
 	
+	$("#test").click(function(e){
+		e.preventDefault();
+		console.log("in click");
+		appendForms();
+	});
+	
+	//append the current list of forms 
+	function appendForms(){
+		console.log("in append");
+		var row = $('<tr></tr>');
+		var col1 = $('<td>adfad</td>'); //form id
+		var col2 = $('<td>adsfdasf</td>'); //title
+		var col3 = $('<td></td>'); //view
+		var form = $('<form action="form" method="get"><input type="hidden" name="formID" value=""></input><button class="btn btn-primary">View</button></form>')
+		col3.append(form);
+		row.append(col1);
+		row.append(col2);
+		row.append(col3);
+		$('tbody').append(row);
+	}
+	
 });
